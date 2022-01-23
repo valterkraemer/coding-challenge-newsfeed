84829947a52b75ad379ad8478b49197741f5732e
Valter Kraemer <valterkraemer@users.noreply.github.com>
2022-01-23 14:51:21 +0200

    Select fellowship

---
+++ components/FellowshipLink.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link"
+import { useRouter } from "next/router"
+import styled from "styled-components"
+
+import { Fellowship } from "types"
+
+type Props = {
+  fellowship: Fellowship;
+}
+
+const labels: Record<Fellowship, string> = {
+  founders: "Founders",
+  angels: "Angels",
+  writers: "Writers",
+}
+
+export default function FellowshipLink({ fellowship }: Props) {
+  const { query } = useRouter()
+
+  return (
+    <Link href={{ query: { fellowship } }}>
+      <Anchor active={query.fellowship === fellowship}>
+        {labels[fellowship]}
+      </Anchor>
+    </Link>
+  )
+}
+
+type AnchorProps = {
+  active: boolean;
+}
+
+const Anchor = styled.a<AnchorProps>`
+  cursor: pointer;
+  font-size: 1.3em;
+  margin-right: 1em;
+
+  ${(props) => props.active && "text-decoration: underline"};
+`
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
+import { Fellowship } from 'types'
+import AlertError from 'components/AlertError'
 import Feed from 'components/Feed'
+import FellowshipLink from 'components/FellowshipLink'
 import Layout from 'components/Layout'
 
 export default function Home() {
+  const { query } = useRouter()
+
+  const fellowship = (query.fellowship || "founders") as Fellowship
+  const validFellowship = ["founders", "angels", "writers"].includes(
+    fellowship
+  )
+
   return (
     <Layout>
       <Head>
@@ -11,7 +22,17 @@ export default function Home() {
       </Head>
       <h1>Newsfeed</h1>
 
-      <Feed fellowship={"founders"} />
+      <div>
+        <FellowshipLink fellowship="founders" />
+        <FellowshipLink fellowship="angels" />
+        <FellowshipLink fellowship="writers" />
+      </div>
+
+      {validFellowship ? (
+        <Feed fellowship={fellowship} />
+      ) : (
+        <AlertError role="alert">Invalid fellowship</AlertError>
+      )}
     </Layout>
   )
 }
